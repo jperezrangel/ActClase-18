08713bdc109c58d656c7d1bfff46657ef7533e6a
jperezrangel <88467921+jperezrangel@users.noreply.github.com>
2021-10-18 12:47:42 -0500

    Ajustes a las Actividad 18.

--- assignments/15SumaColumnas/src/exercise.py
+++ assignments/15SumaColumnas/src/exercise.py
@@ -1,33 +1,12 @@
 def generaMatriz (renglon, columna):
-    matriz = []
-    for i in range (renglon):
-        lista = []
-        for j in range(columna):
-            valor = int(input())
-            lista.append(valor)
-        matriz.append(lista)
-    return matriz
+    pass
 
 def sumaColumnas (matriz):
-    vectorSuma= []
-    for columna  in range(len(matriz[0])):
-        suma = 0
-        for renglon in range(len(matriz)):
-            suma = suma + matriz[renglon][columna]        
-        vectorSuma.append(suma)        
-    return vectorSuma
+    pass
 
 
 def main():
-    renglon = int(input())
-    columna = int(input())
-    if renglon>=1 and columna>=1:
-        matriz = generaMatriz (renglon, columna)
-        vectorSuma = sumaColumnas(matriz)
-        print (vectorSuma)
-    else:
-        print("Error")
-
+    pass
 
 
 if __name__=='__main__':
--- assignments/17MenoresANumero/src/exercise.py
+++ assignments/17MenoresANumero/src/exercise.py
@@ -1,27 +1,11 @@
 def menores_a_10(matriz):
-    menores=[]
-    for renglon in matriz:
-        for elem in renglon:
-            if elem < 10:
-                menores.append(elem)
-    return menores
+    pass
         
 def lee_matriz(ren,col):
-    matriz=[]
-    for i in range(ren):
-        renglon=[]
-        for j in range(col):
-            valor = int(input())
-            renglon.append(valor)
-        matriz.append(renglon)
-    return matriz
+    pass
 
 def main():
-    renglones=int(input())
-    columnas=int(input())
-    matriz=lee_matriz(renglones,columnas)
-    print(menores_a_10(matriz))
-
+    pass
 
 
 if __name__=='__main__':
